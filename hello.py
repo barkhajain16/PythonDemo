b68b137a2bf2e969fe4fd7eb330e2da9b200f5f2
barkhajain16 <49023684+barkhajain16@users.noreply.github.com>
2021-04-30 19:56:04 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-
+import sys
 try:
+    print("Opening browser")
+    print(sys.path)
     driver = webdriver.Chrome("chromedriver.exe")
     driver.get("https://www.google.com")
     
